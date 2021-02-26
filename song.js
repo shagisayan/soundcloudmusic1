e3ab7377be8cf90bb905381ee501b7de03a6863c
shagisayan <kajanshagi@gmail.com>
2021-02-26 23:33:03 +0530

    funwork musicplayer

---
+++ song.js
@@ -0,0 +1,20 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function(){   
+    SC.stream('/tracks/965789977',function(sound){ 
+
+        $('.start1').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+        $('.stop1').click(function(e){
+            e.preventDefault();
+            sound.stop();
+        });
+
+    });
+
+});   
