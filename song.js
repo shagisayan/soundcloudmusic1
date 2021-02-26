1a9dae8322f1ac976139405bf1fa377e3dc64e4f
shagisayan <kajanshagi@gmail.com>
2021-02-27 00:25:09 +0530

    songs update

--- song.js
+++ song.js
@@ -17,5 +17,61 @@ $(document).ready(function(){
 
     });
 
+    SC.stream('/tracks/971575690',function(sound){ 
+
+      $('.start2').click(function(e) {
+        e.preventDefault();
+        sound.start();
+      });
+
+      $('.stop2').click(function(e){
+          e.preventDefault();
+          sound.stop();
+      });
+
+  });
+
+  SC.stream('/tracks/728664046',function(sound){ 
+
+    $('.start3').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+
+    $('.stop3').click(function(e){
+        e.preventDefault();
+        sound.stop();
+    });
+
+});
+
+SC.stream('/tracks/772641595',function(sound){ 
+
+  $('.start4').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('.stop4').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/802934764',function(sound){ 
+
+  $('.start5').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('.stop5').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
 });   
 
