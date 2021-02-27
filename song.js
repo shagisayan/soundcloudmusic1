00cee348d0b1f4e58929d4f7c7a5631b04eed8af
shagisayan <kajanshagi@gmail.com>
2021-02-27 13:33:53 +0530

    english songs updated

--- song.js
+++ song.js
@@ -1,6 +1,7 @@
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
+/* tamil songs*/
 
 $(document).ready(function(){   
     SC.stream('/tracks/801174487',function(sound){ 
@@ -67,6 +68,78 @@ SC.stream('/tracks/802934764',function(sound){
   });
 
   $('.stop5').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+/*english songs */
+
+SC.stream('/tracks/244690563',function(sound){ 
+
+  $('.start6').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('.stop6').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/777072070',function(sound){ 
+
+  $('.start7').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('.stop7').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/807244147',function(sound){ 
+
+  $('.start8').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('.stop8').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/215525311',function(sound){ 
+
+  $('.start9').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('.stop9').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/192032277',function(sound){ 
+
+  $('.start10').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('.stop10').click(function(e){
       e.preventDefault();
       sound.stop();
   });
